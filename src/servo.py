7855960ad7913f050ddf09c8b0b7468ff264025b
Julius Sustarevas <julius1618@gmail.com>
2019-03-15 20:35:29 +0000

    test this

--- src/servo.py
+++ src/servo.py
@@ -2,15 +2,35 @@
 import rospy
 import time
 import math
-from RPIO import PWM
+import RPi.GPIO as GPIO
 from geometry_msgs.msg import Twist
 
 class MotorControl(Object):
 
+	class Servo(Object):
+		def __init__(self,pin,freq=50):		
+			GPIO.setup(pin, GPIO.OUT)
+			self.freq=freq
+			self.pwm=GPIO.PWM(pin, freq)
+			self.pwm.start(0)
+
+		def __del__(self):
+			self.pwm.stop(0)	
+		
+		def set_ms(self, ms):
+			ms=min(max(1000),2000)
+			period=(int)(1000/self.freq)			
+			self.pwm.ChangeDutyCycle(ms/period)	
+
 	def __init__(self):
 		rospy.init_node('motor_control', anonymous=True)
-		self.sub=rospy.Subscriber("cmd_vel", Twist, self.callback) 				
-		self.servo = PWM.Servo()		
+		self.sub=rospy.Subscriber("cmd_vel", Twist, self.callback)
+		GPIO.setmode(GPIO.BOARD)
+		self.left_servo=Servo(17)	
+		self.right_servo=Servo(18)					
+
+	def __del__(self):	
+		GPIO.cleanup()
 
 	def callback(self,msg):
 		# msg.linear.x  # -0.2 0.2 ish m/s
@@ -23,15 +43,10 @@ class MotorControl(Object):
 		ms_value_l=min(2000,max(1000,ms_value_l))
 		ms_value_r=min(2000,max(1000,ms_value_r))		
 		
-		self.servo.set_servo(17, ms_value_l)
-		self.servo.set_servo(18, ms_value_r)
-		
-		# self.servo.stop_servo(18)
-		# self.servo.stop_servo(19)
-
+		self.left_servo.set_servo(ms_value_l)
+		self.right_servo.set_servo(ms_value_r)		
 		rospy.loginfo("desired cmd_vel x is  {}  ang z is  {}".format(msg.linear.x,msg.angular.z))		
 
-if __name__ == '__main__':
-	PWM.setup()
+if __name__ == '__main__':	
 	mt_ctrl=MotorControl()
 	rospy.spin()
